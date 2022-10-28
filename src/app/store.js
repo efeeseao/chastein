051b2cc414f8002ae78a2d725eb98121498497d3
Fernando dos Santos <nando4803@gmail.com>
2022-10-28 01:13:48 +0100

    chore: config api and store

---
+++ src/app/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
+
+import { apiSlice } from './api'
+import authReducer from '../pages/Auth/AuthSlice'
+
+export const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: false
+})
+
+setupListeners(store.dispatch)
